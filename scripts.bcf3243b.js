fa11c4acbb3008f67766dc745985c4718da68375
Rodrigo <rcoto@protonmail.com>
2019-03-04 21:15:47 -0800

    styles/html: Main grid

--- scripts.bcf3243b.js
+++ scripts.bcf3243b.js
@@ -207,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38997" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42923" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
