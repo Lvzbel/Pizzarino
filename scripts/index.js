35632d075437569c1abe177d767daefcbcedb0ed
Rodrigo <rcoto@protonmail.com>
2019-03-23 22:29:32 -0700

    html/css/js: responsve nav link completed it

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,9 @@
 import "../sass/main.scss";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
+const menuButton = document.querySelector(".navigation__menu");
+const menuLinks = document.querySelector(".navigation__links");
+
+menuButton.addEventListener("click", e => {
+  menuLinks.classList.toggle("active");
+});
