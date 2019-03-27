9cb7cc4ed9bcac5d3dedcf25baab57d5c432b03d
Rodrigo <rcoto@protonmail.com>
2019-03-26 19:36:20 -0700

    accessibility: navbar button now is fully accessible with aria role and expandability

--- scripts/index.js
+++ scripts/index.js
@@ -5,5 +5,15 @@ const menuButton = document.querySelector(".navigation__menu");
 const menuLinks = document.querySelector(".navigation__links");
 
 menuButton.addEventListener("click", e => {
+  const expanded = menuButton.getAttribute("aria-expanded");
+
   menuLinks.classList.toggle("hide");
+
+  // Will set accessible value expandable
+  // http://www.davidmacd.com/blog/toggle-aria-expanded-javascript.html
+  if (expanded === "false") {
+    menuButton.setAttribute("aria-expanded", "true");
+  } else if (expanded === "true") {
+    menuButton.setAttribute("aria-expanded", "false");
+  }
 });
